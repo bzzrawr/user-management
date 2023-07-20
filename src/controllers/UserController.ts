6324c605719b2686e7adb85472f1aa6e066ada4c
SYAFIQ ALI OTHMAN <syafiqali.othman@gmail.com>
2023-07-20 19:46:37 +0800

    remove unused userService

--- src/controllers/UserController.ts
+++ src/controllers/UserController.ts
@@ -1,5 +1,4 @@
 import { Request, Response } from 'express';
-import { UserService } from '../services/UserService';
 import dataSource from '../../typeorm.config';
 import { User } from '../entities/User';
 
